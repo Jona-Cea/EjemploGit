00506590a5cd4675fc6bbfec396c5fd4304bd3d7
jona-cea <jonycea7@gmail.com>
2023-09-05 21:42:14 -0600

    firts commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head><title>Mantenedor de clientes</title></head>
+	<body bgcolor="#00FFFF">
+		<center>
+			|| <a href="Agregar.php">Agregar</a>||
+
+
+			|| <a href="Eliminar.php">Eliminar</a>|| 
+			
+
+			|| <a href="Modificar.php">Modificar</a>||
+		
+	</body>
+</html>
